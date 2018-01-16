6c8bbcf35e6fb7ac0b8bc6c0bfaed84e8e56f636
Akash Chandwani <akashchandwani@hotmail.com>
2018-01-16 23:55:19 +0530

    2 test case

--- 2.js
+++ 2.js
@@ -1,8 +1,17 @@
+let counter = 0;
+
 function repeat(operation, num) {
   for (var i = 0; i < num; i++) {
     operation();
   }
 }
 
+function operation(){
+  counter += 1;
+}
+
+repeat(operation, 4);
+
+//console.log("must call operation 4 times ", counter === 4);
 // Do not remove the line below
 module.exports = repeat
