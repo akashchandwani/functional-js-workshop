540004fbd215d66f5db12ad8b5ccf12fff6846b6
Akash Chandwani <akashchandwani@hotmail.com>
2018-01-16 09:53:40 +0530

    1st exercise

---
+++ 1.js
@@ -0,0 +1,7 @@
+
+function upperCaser(input){
+  let upperCaseString = input.toUpperCase();
+  return upperCaseString;
+}
+
+console.log("converts string to upper case", upperCaser("akash chandwani") === "AKASH CHANDWANI");
