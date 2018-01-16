a2d1409d8b158ba5f406f12eb78551a6597daa52
Akash Chandwani <akashchandwani@hotmail.com>
2018-01-17 00:10:55 +0530

    5 with test cases

--- 5.js
+++ 5.js
@@ -3,5 +3,26 @@ function checkUsersValid(goodUsers) {
     return submittedUsers.every(user => goodUsers.some(goodUser => goodUser.id === user.id));
   };
 }
+/*
+let goodUser = [
+  {'id':1},
+  {'id':2},
+  {'id':3},
+  {'id':4}
+]
 
+let submittedUsers = [
+  {'id':1},
+  {'id':2}
+]
+
+console.log("check for all submitted users in goodUser", checkUsersValid(goodUser)(submittedUsers));
+
+submittedUsers = [
+  {'id':1},
+  {'id':55}
+]
+
+console.log("check for extra submitted users", checkUsersValid(goodUser)(submittedUsers));
+*/
 module.exports = checkUsersValid;
