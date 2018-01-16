50e97c19cd6f1e99abbf1469229351b19b91879a
Akash Chandwani <akashchandwani@hotmail.com>
2018-01-16 11:04:26 +0530

    3 with test case

---
+++ 3.js
@@ -0,0 +1,11 @@
+function doubleAll(numbers) {
+  var result = []
+  result = numbers.map(num => num * 2);
+    // result.push(numbers[i] * 2)
+
+  return result;
+}
+
+console.log('multiply each element in array', JSON.stringify(doubleAll([1,2,3]))===JSON.stringify([2,4,6]));
+
+module.exports = doubleAll
